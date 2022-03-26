96c71532fbfeea082e40eb8678d0ea851960207b
eric chang <urbanapartheid@gmail.com>
2022-03-25 21:24:55 -0400

    End of Section 12

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -1,16 +1,32 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace API.DTOs
 {
     public class RegisterDto
     {
         #region Properties
-        [Required]
+        [Required] 
         public string Username { get; set; }
+
+        [Required] 
+        public string KnownAs { get; set; }
+
+        [Required]
+        public string Gender { get; set; }
+
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string Country { get; set; }
 
         [Required]
         [StringLength(8, MinimumLength = 4)]
-        public string Password { get; set; } 
+        public string Password { get; set; }
         #endregion
     }
 }
--- API/DTOs/UserDto.cs
+++ API/DTOs/UserDto.cs
@@ -8,6 +8,10 @@
         public string Token { get; set; } 
 
         public string PhotoUrl { get; set; }
+
+        public string KnownAs { get; set; }
+
+        public string Gender { get; set; }
         #endregion
     }
 }
